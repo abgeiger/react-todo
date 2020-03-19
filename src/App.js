11111103f4eef6ec55df57c59616402b6d0de7c7
abgeiger <30698092+abgeiger@users.noreply.github.com>
2020-03-19 10:32:26 -0500

    Added delete functionality.

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
 
   // Delete Todo
   delTodo = (id) => {
-    console.log(id)
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
   }
 
   render() {
